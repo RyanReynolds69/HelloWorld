19c520ed8bee9ec069ffbc06437931c0ad3bde68
RyanReynolds69 <w.roelofsen@student.avans.nl>
2026-09-10 13:36:11 +0200

    pls werk gewoon

--- helloworld.c
+++ helloworld.c
@@ -4,7 +4,8 @@ int main()
 {
     printf("Hello, World!\n");
     printf("Dab on it Wagwan");
-    printf("( . Y . )\n");
+    printf("( . Y . )\n"); 
+    printf("hello gamemeister");
 
     return 0;
 }
